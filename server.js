ef6a4ce571b7896fe15ba4262bf393c530252670
Prem Singh <22cspre036@ldce.ac.in>
2023-11-11 16:44:09 +0530

    admin dash board saved

--- server.js
+++ server.js
@@ -38,7 +38,7 @@ app.get("/signup", (req, res) =>
   res.sendFile(path.join(__dirname, "/frontend/build/index.html"))
 );
 
-app.get("*", (req, res) =>
+app.get("/admin/*", (req, res) =>
   res.sendFile(path.join(__dirname, "/frontend/build/index.html"))
 );
 
