17c9a23a0b2ddbb8b88c643d53f75e922bfbea19
Prem Singh <22cspre036@ldce.ac.in>
2023-11-11 16:36:09 +0530

    static file path set

--- server.js
+++ server.js
@@ -38,7 +38,7 @@ app.get("/signup", (req, res) =>
   res.sendFile(path.join(__dirname, "/frontend/build/index.html"))
 );
 
-app.get("/admin", (req, res) =>
+app.get("*", (req, res) =>
   res.sendFile(path.join(__dirname, "/frontend/build/index.html"))
 );
 
