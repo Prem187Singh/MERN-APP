e7c3bb09762ecfbf2796db64bb8abfea2eb30c94
Prem Singh <22cspre036@ldce.ac.in>
2023-11-20 18:42:02 +0530

    static file served

--- server.js
+++ server.js
@@ -21,26 +21,18 @@ mongoose
   });
 
 const app = express();
+const __dirname = path.resolve();
+app.use(express.static(path.join(__dirname, "/frontend/build")));
 app.use(cors());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-const __dirname = path.resolve();
-app.use(express.static(path.join(__dirname, "/frontend/build")));
+
 app.get("/", (req, res) =>
   res.sendFile(path.join(__dirname, "/frontend/build/index.html"))
 );
-app.get("/signin", (req, res) =>
-  res.sendFile(path.join(__dirname, "/frontend/build/index.html"))
-);
-app.get("/signup", (req, res) =>
-  res.sendFile(path.join(__dirname, "/frontend/build/index.html"))
-);
 
-app.get("/admin/*", (req, res) =>
-  res.sendFile(path.join(__dirname, "/frontend/build/index.html"))
-);
 
 app.get("/api/keys/paypal", (req, res) => {
   res.send(process.env.PAYPAL_CLIENT_ID || "sb");
@@ -60,10 +52,12 @@ app.use("/api/orders", orderRouter);
 app.use((err, req, res, next) => {
   res.status(500).send({ message: err.message });
 });
-
+app.get("*", (req, res) =>
+  res.sendFile(path.join(__dirname, "/frontend/build/index.html"))
+);
 const port = process.env.PORT || 4000;
 app.listen(port, () => {
-
+console.log("server started")
 });
 
 
